1428c5c2d5e23f8bfb02ed994025b6112ae29e6a
ps3296 <ps3296@nyu.edu>
2017-12-04 18:24:13 -0500

    Update HADEVELOPT.py

--- ColumnCleaning/HADEVELOPT_CLEANING/HADEVELOPT.py
+++ ColumnCleaning/HADEVELOPT_CLEANING/HADEVELOPT.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
 
     columnData = lines.map(lambda x: (x[0], x[18], check_datatype(x[18]), validity(x[18])))
     #columnData = columnData.filter(lambda x: x[3] == "VALID") #This line is used to remove the identified invalid entires from the column
-    columnData.saveAsTextFile("col18.out")
+    columnData.saveAsTextFile("col19.out")
 
     lines = lines.map(lambda x: (validity(x[18]), 1)).reduceByKey(lambda x, y: x + y).collect()
     lines = sc.parallelize(lines)
-    lines.saveAsTextFile("col18Stats.out")
+    lines.saveAsTextFile("col19Stats.out")
 
     sc.stop()
