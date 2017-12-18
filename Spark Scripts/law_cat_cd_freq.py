b0e0b509cffe8f6b83dea391de425d7ba158d320
ps3296 <priyanshishah@Priyanshis-MacBook-Pro.local>
2017-12-17 23:02:44 -0500

    level of offense analysis script added

---
+++ Spark Scripts/law_cat_cd_freq.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+from pyspark import SparkContext
+from csv import reader
+import sys
+
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    lines = sc.textFile(sys.argv[1], 1)
+    lines = lines.mapPartitions(lambda x: reader(x))
+
+    def validity(x):
+            if x is "" or x is " ":
+                return "INVALID"
+            else :
+                return x
+
+
+    header = lines.first()    #extract header
+
+    lines = lines.filter(lambda x: x != header)
+    lines = lines.filter(lambda x: (str(x[13]) == "BROOKLYN"))
+
+    valid_Invalid_Null = lines.map(lambda x: (validity(x[11]), 1)).reduceByKey(lambda x, y: x + y)
+
+    valid_Invalid_Null = valid_Invalid_Null.filter(lambda x: x[0] != "INVALID")
+
+
+    counts = valid_Invalid_Null.map(
+        lambda x: str(x[0]).replace("'", "").replace('(', '').replace(')', '') + '\t' + str(x[1]))
+
+    counts.saveAsTextFile("law_cat_cd_freq.out")
+    valid_Invalid_Null = valid_Invalid_Null.collect()
+    sc.stop()
